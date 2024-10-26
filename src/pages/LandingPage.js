b944cf25414a9718bef26e55767971462cf7ccc0
Valeska <valeskaleandro@gmail.com>
2024-10-26 14:41:27 -0300

    feat: create landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,7 +3,23 @@ import '../styles/LandingPage.scss';
 
 const LandingPage = () => {
   return (
-    <div></div>
+    <div className='landing-container'>
+      <div className='hero'>
+        <img className='hero-image' alt='Celular aberto no linkedIn' src='/images/mockup.png'/>
+        <div className='hero-title'>
+          <h2>
+            Menos tempo postando <br />
+            mais tempo <span>se conectando</span>    
+          </h2>
+        </div>
+      </div>
+      <div className='slog'>
+        <p>
+          A única plataforma para automatizar <br />
+          as suas postagens no <span>LinkedIn</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
