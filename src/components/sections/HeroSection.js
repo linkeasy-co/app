4b7926a3bc0d81fab4bc8f16e0b3e437e2242bd2
giovanni cabral <giolucacab@outlook.com>
2024-12-04 20:01:56 -0300

    sobe

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -15,21 +15,21 @@ const HeroSection = () => {
         <div className='hero-title'>
           <h2>
             Automatiza agora suas <br />
-            postagens no LinkedIn  
+            postagens no LinkedIn
           </h2>
         </div>
         <div className='cta'>
           <p>De <span className='old-price'>{formatCurrency(OLD_PRICE)}</span></p>
           <p>Por apenas <span className='new-price'>{formatCurrency(NEW_PRICE)}</span> por mês</p>
-        </div> 
+        </div>
         <ButtonDefault title='Assine agora' icon={FaArrowRight} onClick={() => navigate('/compra')} className='lg animation' />
       </div>
 
       <div className='slog'>
-        <p>Linkeasy usa IA para transformar você em uma referência no LinkedIn, criando conteúdos
+        <p>Likify usa IA para transformar você em uma referência no LinkedIn, criando conteúdos
         personalizados para você. Sua presença ativa e estratégica na maior rede de negócios.</p>
       </div>
-      
+
     </div>
   )
 }
